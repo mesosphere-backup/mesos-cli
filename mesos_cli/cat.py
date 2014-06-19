aab8e364e41efa68508c652cd768c79a0de6628c
Thomas Rampelberg <thomas@saunter.org>
2014-06-18 18:14:00 -0700

    Matching normal cat multi-file operation

--- mesos_cli/cat.py
+++ mesos_cli/cat.py
@@ -9,25 +9,27 @@ from . import slave_file
 from . import task
 
 parser = cli.parser(
-    description="cat a file from a specific task"
+    description="concatenate and print files"
 )
 
 parser.add_argument(
-    'task', type=str,
-    help="ID of the task. Must match a specific task."
+    'task',
+    help="ID of the task. May match multiple tasks (or all)"
 )
 
 parser.add_argument(
-    'file', type=str, nargs="?", default="stdout",
+    'file', type=str, nargs="*", default=["stdout"],
     help="Path to the file inside the task's sandbox."
 )
 
 def main():
     cfg, args, m = cli.init(parser)
 
-    t = master.task(m, args.task)
-    s = master.slave(m, t["slave_id"])
-    p = os.path.join(task.directory(m, t), args.file)
+    for t in master.tasks(m, args.task):
+        s = master.slave(m, t["slave_id"])
+        d = task.directory(m, t)
+        for f in args.file:
+            p = os.path.join(d, f)
 
-    for chunk in slave_file.SlaveFile(s, p):
-        print chunk
+            for chunk in slave_file.SlaveFile(s, p):
+                print chunk
