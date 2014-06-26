de510fa10c0cb54950500c706d9d2b55c4343573
Thomas Rampelberg <thomas@saunter.org>
2014-06-26 15:11:57 -0700

    adding scp tests

---
+++ tests/integration/test_scp.py
@@ -0,0 +1,37 @@
+
+import mock
+
+import mesos_cli.scp
+
+from .. import utils
+
+class TestScp(utils.MockState):
+
+    @utils.patch_args([
+        "mesos-scp",
+        "stdout",
+        "/tmp"
+    ])
+    def test_single(self):
+        with mock.patch("gevent.subprocess.check_call", return_value=0) as m:
+            mesos_cli.scp.main()
+
+            m.assert_called_with(
+                [ "scp", "-pr", "stdout", "10.141.141.10:/tmp" ])
+            assert len(self.lines) == 3
+            assert "uploaded" in self.stdout
+
+    @utils.patch_args([
+        "mesos-scp",
+        "stdout",
+        "stderr",
+        "/tmp"
+    ])
+    def test_multiple(self):
+        with mock.patch("gevent.subprocess.check_call", return_value=0):
+            mesos_cli.scp.main()
+
+            assert len(self.lines) == 5
+            assert "uploaded" in self.stdout
+
+
