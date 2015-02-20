0db9f6c77e8ddd24ffad3ae5fb6c2b8c1be321c6
Mahdi Ben Hamida <mahdouch@gmail.com>
2015-02-20 14:33:47 -0800

    added task stats

--- mesos/cli/cmds/frameworks.py
+++ mesos/cli/cmds/frameworks.py
@@ -20,7 +20,7 @@ from __future__ import absolute_import, print_function
 import blessings
 import prettytable
 
-from .. import cli, exceptions, parallel, util
+from .. import cli
 from ..master import CURRENT as MASTER
 
 try:
@@ -34,20 +34,47 @@ parser = cli.parser(
 )
 
 parser.add_argument(
+    "-b", "--task-breakdown", action="store_true",
+    help="show task breakdown per framework"
+)
+
+parser.add_argument(
     "-i", "--inactive", action="store_true",
     help="show inactive frameworks as well"
 )
 
+
+def task_stats(tasks, state):
+    count = 0
+    for task in tasks:
+        if task['state'] == state:
+            count += 1
+    return count
+
 @cli.init(parser)
 def main(args):
     term = blessings.Terminal()
-    
-    #print(args.inactive)
 
     table_generator = OrderedDict([
         ("ID", lambda x: x["id"]),
         ("name", lambda x: x["name"]),
+        ("host", lambda x: x["hostname"]),
+        ("active", lambda x: x["active"]),
+        ("active tasks", lambda x: len(x["tasks"])),
     ])
+
+    if args.task_breakdown:
+        # active tasks
+        table_generator['staging'] = lambda x: task_stats(x["tasks"], "TASK_STAGING")
+        table_generator['starting'] = lambda x: task_stats(x["tasks"], "TASK_STARTING")
+        table_generator['running'] = lambda x: task_stats(x["tasks"], "TASK_RUNNING")
+        # terminal state tasks. The count here is approximate since the API caps the result
+        # at a max of 1000 tasks
+        table_generator['failed'] = lambda x: task_stats(x["completed_tasks"], "TASK_FAILED")
+        table_generator['killed'] = lambda x: task_stats(x["completed_tasks"], "TASK_FAILED")
+        table_generator['error'] = lambda x: task_stats(x["completed_tasks"], "TASK_ERROR")
+        table_generator['lost'] = lambda x: task_stats(x["completed_tasks"], "TASK_ERROR")
+        table_generator['finished'] = lambda x: task_stats(x["completed_tasks"], "TASK_FINISHED")
 
     tb = prettytable.PrettyTable(
         [x.upper() for x in table_generator.keys()],
@@ -63,8 +90,9 @@ def main(args):
         return [fn(framework) for fn in table_generator.values()]
 
     for framework in MASTER.state['frameworks']:
-        tb.add_row(format_row(framework))
-
+        if args.inactive or framework['active']:
+            tb.add_row(format_row(framework))
+ 
     if tb.rowcount == 0:
         print("===>{0}You have no frameworks{1}<===".format(
             term.red, term.white))
